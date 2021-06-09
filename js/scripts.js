8a291c0a3b074df90c897b5e1a2b1bc3da1910b2
Alex Bertotto <ajb5206@gmail.com>
2021-06-08 19:54:13 -0700

    initial css, img, js, and html files

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+jQuery("h1").click(function() {
+  alert("this is a heading.");
+});
+
+jQuery("p").click(function(){
+  alert("this is a paragraph.");
+});
+
+jQuery("img").click(function(){
+  alert("this is an image.");
+});
